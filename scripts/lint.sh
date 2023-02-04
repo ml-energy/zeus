75c489240fde989bfe1883ed52461722c5c948cd
Jae-Won Chung <jwnchung@umich.edu>
2023-02-04 02:12:45 -0500

    Chore: Add scripts/lint.sh

---
+++ scripts/lint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -ev
+
+if [[ -z $GITHUB_ACTION ]]; then
+  black --exclude examples/cifar100/models zeus capriccio examples
+else
+  black --check --exclude examples/cifar100/models zeus capriccio examples
+fi
+
+pydocstyle --match-dir='!examples/cifar100/models' zeus capriccio examples
+pylint zeus
