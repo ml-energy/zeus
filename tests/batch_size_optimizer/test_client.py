ed0bc75000447d6206166249fbd5ce8bcfc8b206
Yong Seung Lee <yongseunglee@Yongs-MacBook-Air.local>
2024-02-12 20:22:58 -0500

    simple client test

--- tests/batch_size_optimizer/test_client.py
+++ tests/batch_size_optimizer/test_client.py
@@ -16,7 +16,7 @@ import pytest
 import httpx
 from pytest_mock import MockerFixture
 from tests.test_monitor import mock_gpus, pynvml_mock
-from zeus.monitor.energy import ZeusMonitor
+from zeus.monitor.energy import Measurement, ZeusMonitor
 from zeus.optimizer.batch_size.client import BatchSizeOptimizerClient
 from zeus.optimizer.batch_size.server.models import JobSpec
 
@@ -50,6 +50,7 @@ def client():
         yield c
 
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 @pytest.fixture
 def mock_monitor(mocker: MockerFixture):
@@ -89,25 +90,50 @@ def test_register_job(client, mocker: MockerFixture):
     zeus_monitor_mock_instance.nvml_gpu_indices = [0]
     zeus_monitor_mock_instance.gpu_indices = [0]
 >>>>>>> 9a91219 (checkpoint - testing client)
+=======
+@pytest.fixture
+def mock_monitor(mocker: MockerFixture):
+    mocker.patch("pynvml.nvmlInit")
+
+    zeus_monitor_mock_instance = MagicMock(spec=ZeusMonitor)
+    zeus_monitor_mock_instance.nvml_gpu_indices = [0, 1, 2, 3]
+    zeus_monitor_mock_instance.gpu_indices = [0, 1, 2, 3]
+    zeus_monitor_mock_instance.end_window.return_value = Measurement(
+        time=37.24807469360,
+        energy={
+            0: 4264.87199999392,
+            1: 4367.186999991536,
+            2: 4342.869000002742,
+            3: 4158.034000009298,
+        },
+    )
+>>>>>>> 8aa3b04 (simple client test)
 
     mocker.patch(
         "zeus.monitor.energy.ZeusMonitor", return_value=zeus_monitor_mock_instance
     )
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
 >>>>>>> 9a91219 (checkpoint - testing client)
+=======
+>>>>>>> 8aa3b04 (simple client test)
     mocker.patch("pynvml.nvmlDeviceGetHandleByIndex").return_value = 0
     mocker.patch("pynvml.nvmlDeviceGetPowerManagementLimitConstraints").return_value = (
         300
     )
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 8aa3b04 (simple client test)
     return zeus_monitor_mock_instance
 
 
 @pytest.mark.anyio
 def test_register_job(client, mock_monitor, mocker: MockerFixture):
     mocker.patch("httpx.post", side_effect=client.post)
+<<<<<<< HEAD
     job = JobSpec.parse_obj(fake_job)
     bso_client = BatchSizeOptimizer(mock_monitor, "", job)
     assert bso_client.max_power == 300 * len(mock_monitor.gpu_indices)
@@ -185,3 +211,42 @@ def test_converge_fail(client, mock_monitor, mocker: MockerFixture):
     bso_client = BatchSizeOptimizerClient(zeus_monitor_mock_instance, "", job)
     assert bso_client.max_power == 300
 >>>>>>> 9a91219 (checkpoint - testing client)
+=======
+    job = JobSpec.parse_obj(fake_job)
+    bso_client = BatchSizeOptimizerClient(mock_monitor, "", job)
+    assert bso_client.max_power == 300 * len(mock_monitor.gpu_indices)
+    bso_client = BatchSizeOptimizerClient(mock_monitor, "", job)
+    assert bso_client.max_power == 300 * len(mock_monitor.gpu_indices)
+
+
+@pytest.mark.anyio
+def test_batch_sizes(client, mock_monitor, mocker: MockerFixture):
+    mocker.patch("httpx.post", side_effect=client.post)
+    mocker.patch("httpx.get", side_effect=client.get)
+    job = JobSpec.parse_obj(fake_job)
+    bso_client = BatchSizeOptimizerClient(mock_monitor, "", job)
+    bs = bso_client.get_batch_size()
+
+    assert bs == 1024 and bso_client.current_batch_size == 1024
+
+    bso_client.on_train_begin()
+    bso_client.on_evaluate(0.1)
+    bso_client.on_evaluate(0.2)
+    bso_client.on_evaluate(0.6)  # Converged
+    bs = bso_client.get_batch_size()
+
+    assert bs == 512 and bso_client.current_batch_size == 512
+
+    i = 0
+    with pytest.raises(Exception) as e_info:
+        while i < job.max_epochs + 10:
+            bso_client.on_evaluate(0.3)
+            i += 1
+            assert i == bso_client.cur_epoch
+            assert bso_client.current_batch_size == 512
+
+    assert str(e_info.value) == f"Couldn't converge within max_epoch({job.max_epochs})"
+    bs = bso_client.get_batch_size()
+
+    assert bs == 2048 and bso_client.current_batch_size == 2048
+>>>>>>> 8aa3b04 (simple client test)
