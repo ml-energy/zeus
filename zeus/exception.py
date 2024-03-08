1543e7a5228b443028c1693ad342044eaf3b6f23
Parth Raut <praut@umich.edu>
2024-03-08 16:24:45 -0500

    fixed lint

--- zeus/exception.py
+++ zeus/exception.py
@@ -1,9 +1,14 @@
-class ZeusBaseException(Exception):
-    """Zeus base exception class.""" 
+"""Base Zeus Exception Class."""
+
+
+class ZeusBaseError(Exception):
+    """Zeus base exception class."""
 
     def __init__(self, message: str) -> None:
+        """Initialize Base Zeus Exception."""
         self.message = message
         super().__init__(message)
 
     def __str__(self) -> str:
+        """Return message."""
         return self.message
