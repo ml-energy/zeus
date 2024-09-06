54e421af337575438527c9e360a2d09a7df84478
danielhou0515 <71229877+danielhou0515@users.noreply.github.com>
2024-09-06 07:28:22 -0700

    Feat: Utility function to retrieve CPU index from PID (#117)
    Co-authored-by: Jae-Won Chung <jwnchung@umich.edu>

--- zeus/device/cpu/__init__.py
+++ zeus/device/cpu/__init__.py
@@ -6,10 +6,34 @@ which returns a CPU Manager object specific to the platform.
 
 from __future__ import annotations
 
+import os
+from typing import Literal
+
 from zeus.device.cpu.common import CPUs, ZeusCPUInitError
 from zeus.device.cpu.rapl import rapl_is_available, RAPLCPUs
 
 _cpus: CPUs | None = None
+
+
+def get_current_cpu_index(pid: int | Literal["current"] = "current") -> int:
+    """Retrieves the specific CPU index (socket) where the given PID is running.
+
+    If no PID is given or pid is "current", the CPU index returned is of the CPU running the current process.
+
+    !!! Note
+        Linux schedulers can preempt and reschedule processes to different CPUs. To prevent this from happening
+        during monitoring, use `taskset` to pin processes to specific CPUs.
+    """
+    if pid == "current":
+        pid = os.getpid()
+
+    with open(f"/proc/{pid}/stat") as stat_file:
+        cpu_core = int(stat_file.read().split()[38])
+
+    with open(
+        f"/sys/devices/system/cpu/cpu{cpu_core}/topology/physical_package_id"
+    ) as phys_package_file:
+        return int(phys_package_file.read().strip())
 
 
 def get_cpus() -> CPUs:
