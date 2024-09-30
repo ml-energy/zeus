4be7f4993941e6feb67e001225f1b627bdc0a57f
Jae-Won Chung <jwnchung@umich.edu>
2024-09-30 16:20:18 -0400

    [Docs] Add `BUILD_SOCIAL_CARD` env, skip social card build by default (#130)

--- scripts/preview_docs.sh
+++ scripts/preview_docs.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# This script builds a local version of the documentation and makes it available at localhost:7777.
+# By default it does not build social preview cards. If you want to debug social cards,
+# set the environment variable `BUILD_SOCIAL_CARD=true` to this script.
+
 pip list | grep mkdocs-material 2>&1 >/dev/null
 
 if [[ ! $? -eq 0 ]]; then
