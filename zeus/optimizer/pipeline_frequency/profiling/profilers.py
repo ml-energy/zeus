ea2c417a9aed95fb75125a42db9e94c4840ac9ce
DdIiVvYyAaMm <divyams@umich.edu>
2025-02-25 01:15:08 -0500

    Adding Instruction Profiler with models and interfaces in current scheduler

---
+++ zeus/optimizer/pipeline_frequency/profiling/profilers.py
@@ -0,0 +1,193 @@
+"""Schedulers that are really profilers."""
+
+from __future__ import annotations
+
+from typing import Generator
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from fastapi.logger import logger
+
+from zeus.optimizer.pipeline_frequency.profiling.models import (
+    PerseusSettings,
+    JobInfo,
+    RankInfo,
+    PowerStateSchedule,
+    ProfilingResult,
+    PipeInstruction,
+)
+from zeus.optimizer.pipeline_frequency.server.scheduler import PowerStateSchedulerV2, make_3d_parallel
+
+
+class InstructionProfiler(PowerStateSchedulerV2):
+    """Profiles the time and energy of each instruction.
+
+    Average time and energy consumption of each instruction on each stage for each power state.
+    """
+
+    def __init__(
+        self,
+        job_info: JobInfo,
+        rank_infos: list[RankInfo],
+        perseus_settings: PerseusSettings,
+        warmup_step_ratio: float = 0.1,
+        minimum_power_state: int = 850,
+    ) -> None:
+        """Initialize the scheduler.
+
+        Args:
+            job_info: Info about the training job.
+            rank_infos: Info about all ranks. May not be sorted in rank order.
+            perseus_settings: PerseusSettings object.
+            warmup_step_ratio: The ratio of warmup steps over the total number of steps within one
+                profiling window, ranging from 0 to 1. (Default: `0.1`)
+            minimum_power_state: The minimum power state to profile. (Default: `850`)
+        """
+        super().__init__(job_info, rank_infos, perseus_settings)
+
+        assert (
+            self.perseus_settings.dump_data
+        ), "InstructionProfiler is designed to dump profiling data."
+
+        self.warmup_step_ratio = warmup_step_ratio
+        self.minimum_power_state = minimum_power_state
+        self._header = ["stage", "instruction", self.mode, "time", "energy"]
+        self._records: list[tuple[int, PipeInstruction, int, float, float]] = []
+
+    def _run(self) -> Generator[list[PowerStateSchedule], list[ProfilingResult], None]:
+        # Warm up the GPUs.
+        max_power_state = max(self.power_state_range)
+        for _ in range(2):
+            _ = yield [
+                PowerStateSchedule(
+                    rank=rank_info.rank,
+                    power_states=[max_power_state for _ in rank_info.pipe_schedule],
+                )
+                for rank_info in self.rank_infos
+            ]
+        # Run each power state one at a time.
+        for power_state in sorted(self.power_state_range, reverse=True):
+            if power_state < self.minimum_power_state:
+                break
+            profiling_results = yield [
+                PowerStateSchedule(
+                    rank=rank_info.rank,
+                    power_states=[power_state for _ in rank_info.pipe_schedule],
+                )
+                for rank_info in self.rank_infos
+            ]
+            for stage, prof in enumerate(profiling_results):
+                # for inst in prof.time_breakdown:
+                for inst in [PipeInstruction.FORWARD, PipeInstruction.BACKWARD]:
+                    inst_time = prof.time_breakdown[inst]
+                    inst_energy = prof.energy_breakdown[inst]
+                    assert len(inst_time) == len(
+                        inst_energy
+                    ), "Number of iterations for time and energy should be the same"
+                    warmup_steps = max(int(len(inst_time) * self.warmup_step_ratio), 1)
+                    time_arr = np.array(inst_time[warmup_steps:]).flatten()
+                    energy_arr = np.array(inst_energy[warmup_steps:]).flatten()
+                    self._records.append(
+                        (
+                            stage,
+                            inst,
+                            power_state,
+                            time_arr.mean().item(),
+                            energy_arr.mean().item(),
+                        )
+                    )
+
+            # After collecting the result of each power state, save results.
+            # This will keep overwriting the same file.
+            df = pd.DataFrame.from_records(data=self._records, columns=self._header)
+            info = self.job_info
+            filepath = (
+                f"{self.perseus_settings.dump_dir}/{info.job_id}/"
+                f"{info.framework}+{info.model_name}+{info.partition_method}"
+                f"+dp{info.dp_degree}"
+                f"+tp{info.tp_degree}"
+                f"+pp{info.pp_degree}"
+                f"+mbs{info.microbatch_size}"
+                f"+nmb{info.num_microbatches}"
+                ".csv"
+            )
+            df.to_csv(filepath, index=False)
+            logger.info("Saved profiling result to %s", filepath)
+
+        # Training should stop after profiling is done. Raise an error.
+        raise RuntimeError("[profiling-done] Profiling is done, so stop training!")
+
+
+class PointSolution(PowerStateSchedulerV2):
+    """Reads in point solutions and just executes them."""
+
+    def __init__(
+        self,
+        job_info: JobInfo,
+        rank_infos: list[RankInfo],
+        perseus_settings: PerseusSettings,
+        warmup_iters: int = 2,
+        solution_path: str = "frequencies.py",
+        reverse: bool = False,
+    ) -> None:
+        """Initialize the scheduler.
+
+        Args:
+            job_info: Info about the training job.
+            rank_infos: Info about all ranks. May not be sorted in rank order.
+            perseus_settings: PerseusSettings object.
+            warmup_iters: Number of iterations to warm up the GPUs with all max power states.
+            solution_path: Path to the solution file or directory.
+                If a directory is given, all .py files inside will be run in alphabetical order.
+            reverse: If True, reverse the order of the solutions.
+        """
+        super().__init__(job_info, rank_infos, perseus_settings)
+
+        self.warmup_iters = warmup_iters
+        self.solution_path = Path(solution_path)
+        self.solutions: list[list[PowerStateSchedule]] = []
+
+        # If the solution path is a directory, we want to run everything in that directory.
+        # If not, we just run the one file.
+        if self.solution_path.is_dir():
+            for file in sorted(self.solution_path.iterdir(), reverse=reverse):
+                if file.suffix == ".py":
+                    self.solutions.append(self._read_solution(file))
+        elif self.solution_path.is_file() and self.solution_path.suffix == ".py":
+            self.solutions.append(self._read_solution(self.solution_path))
+        else:
+            raise ValueError(
+                f"Solution path '{self.solution_path}' is not a directory or a .py file."
+            )
+
+    def _read_solution(self, solution_path: Path) -> list[PowerStateSchedule]:
+        """Read in a solution from a file and convert to a power state schedule."""
+        # pylint: disable=eval-used
+        with open(solution_path, encoding="utf-8") as f:
+            solution = eval(f.read())
+
+        # Convert the solution to a list of power states.
+        return self._config_to_schedule(solution)
+
+    def _run(self) -> Generator[list[PowerStateSchedule], list[ProfilingResult], None]:
+        # Warm up the GPUs.
+        max_power_state = max(self.power_state_range)
+        for _ in range(self.warmup_iters):
+            _ = yield [
+                PowerStateSchedule(
+                    rank=rank_info.rank,
+                    power_states=[max_power_state for _ in rank_info.pipe_schedule],
+                )
+                for rank_info in self.rank_infos
+            ]
+
+        # Run each solution one at a time.
+        for solution in self.solutions:
+            _ = yield solution
+
+        # Stop running after all solutions are done.
+        raise RuntimeError("[profiling-done] All solutions are done, so stop training!")
+
+
+PointSolution3D = make_3d_parallel(PointSolution)
--- zeus/optimizer/pipeline_frequency/server/scheduler.py
+++ zeus/optimizer/pipeline_frequency/server/scheduler.py
@@ -6,7 +6,8 @@ import copy
 from pathlib import Path
 from contextlib import suppress
 from abc import ABC, abstractmethod
-from typing import Callable, Generator, Sequence, Type
+from typing import Callable, Literal, Generator, Sequence, Type
+from fastapi.logger import logger
 
 from zeus.optimizer.pipeline_frequency.common import (
     PFOServerSettings,
@@ -16,6 +17,16 @@ from zeus.optimizer.pipeline_frequency.common import (
     ProfilingResult,
 )
 from zeus.utils.logging import get_logger
+
+from zeus.optimizer.pipeline_frequency.profiling.models import (
+    PerseusSettings,
+    JobInfo,
+    RankInfo,
+    PowerStateSchedule,
+    ProfilingResult,
+    PipeInstruction,
+)
+
 
 logger = get_logger(__name__)
 
@@ -276,3 +287,287 @@ class PointSolution(FrequencyScheduler):
 
 
 PointSolution3D = make_3d_parallel(PointSolution)
+
+
+"""Interfaces for defining power state schedulers."""
+
+
+class PowerStateScheduler(ABC):
+    """Interface for classes that enclose power state scheduling policies."""
+
+    mode: Literal["frequency", "power limit"]
+
+    def __init__(
+        self,
+        job_info: JobInfo,
+        rank_infos: list[RankInfo],
+        perseus_settings: PerseusSettings,
+    ) -> None:
+        """Initialize the scheduler.
+
+        Args:
+            job_info: Info about the training job.
+            rank_infos: Info about all ranks. May not be sorted in rank order.
+            perseus_settings: PerseusSettings object.
+        """
+        self.job_info = job_info
+        self.rank_infos = sorted(rank_infos, key=lambda info: info.rank)
+        # Currently we assume that all ranks have the same power states available.
+        self.power_state_range = self.rank_infos[0].power_state_range
+        self.world_size = self.job_info.world_size
+        self.perseus_settings = perseus_settings
+
+    def _config_to_schedule(self, config: list[list[int]]) -> list[PowerStateSchedule]:
+        schedules = []
+        for stage_id in range(self.job_info.pp_degree):
+            schedule = []
+            config_iter = iter(config[stage_id])
+            power_state = config[stage_id][0]
+            num_compute_insts = 0
+            for inst in self.rank_infos[stage_id].pipe_schedule:
+                if inst in [PipeInstruction.FORWARD, PipeInstruction.BACKWARD]:
+                    power_state = next(config_iter)
+                    num_compute_insts += 1
+                schedule.append(power_state)
+            if num_compute_insts != len(config[stage_id]):
+                raise RuntimeError(
+                    f"Number of compute instructions in the pipe schedule ({num_compute_insts}) "
+                    f"does not match the number of power states in the config ({len(config[stage_id])})",
+                )
+            schedules.append(PowerStateSchedule(rank=stage_id, power_states=schedule))
+        return schedules
+
+    @abstractmethod
+    def observe(self, profiling_results: list[ProfilingResult]) -> None:
+        """Ingest the profiling results for the previous schedule.
+
+        Args:
+            profiling_results: Doesn't have to be sorted in rank order.
+        """
+
+    @abstractmethod
+    def next_schedule(self) -> list[PowerStateSchedule]:
+        """Return the schedules for the next round of iterations.
+
+        Returns:
+            A list of `PowerStateSchedule`s. May not be sorted in rank order.
+        """
+
+
+class PowerStateSchedulerV2(PowerStateScheduler):
+    """Interface for classes that enclose power state scheduling policies."""
+
+    def __init__(
+        self,
+        job_info: JobInfo,
+        rank_infos: list[RankInfo],
+        perseus_settings: PerseusSettings,
+    ) -> None:
+        """Initialize the scheduler.
+
+        Args:
+            job_info: Info about the training job.
+            rank_infos: Info about all ranks. May not be sorted in rank order.
+            perseus_settings: PerseusSettings object.
+        """
+        super().__init__(job_info, rank_infos, perseus_settings)
+        self._generator = self._run()
+        self._next_schedule: list[PowerStateSchedule] | None = None
+
+    def observe(self, profiling_results: list[ProfilingResult]) -> None:
+        """Ingest the profiling results for the previous schedule.
+
+        Args:
+            profiling_results: Doesn't have to be sorted in rank order.
+        """
+        # When there are no more schedules left to yield, the generator will
+        # raise `StopIteration`. We just ignore this, and later invocations of
+        # `next_schedule()` will return the last schedule returned forever.
+        with suppress(StopIteration):
+            self._next_schedule = self._generator.send(profiling_results)
+
+    def next_schedule(self) -> list[PowerStateSchedule]:
+        """Return the schedules for the next round of iterations.
+
+        Returns:
+            A list of `PowerStateSchedule`s. May not be sorted in rank order.
+        """
+        if self._next_schedule is None:
+            try:
+                self._next_schedule = next(self._generator)
+            except StopIteration as exc:
+                raise RuntimeError(
+                    "The _run generator raised StopIteration on its first next call.",
+                ) from exc
+        return self._next_schedule
+
+    @abstractmethod
+    def _run(self) -> Generator[list[PowerStateSchedule], list[ProfilingResult], None]:
+        """Yield next schedules and receives profiling results in one place.
+
+        This is an alternative way to write a power state scheduler. The advantage is
+        that everything is enclosed inside this method. The downside is that you'll
+        have to read this and understand how this generator works.
+
+        The following implementation is a simple example of writing a scheduler using
+        this class. `yield` the next power state schedule, and receive the profiling
+        results corresponding to that schedule from the `yield`. `observe` and
+        `next_schedule` will run the generator for you.
+
+        In general, this generator should be designed to `yield` schedules infinitely.
+        However, if this was written to write a finite number of next schedules and
+        raise `StopIteration`, the last schedule cached inside `self._next_schedule`
+        will infinitely be returned from the call to `next_schedule`. This can be
+        useful when you converge to the optimal schedule and stop the generator, and
+        the rest of training will run with the final optimal schedule indefinitely.
+        """
+        while True:
+            # Generate the next power state schedule
+            next_schedule: list[PowerStateSchedule] = []
+            # Send the next schedule to client and receive the profiling result from client
+            profiling_results = yield next_schedule
+            # Ingest the profiling result
+            logger.debug("%s", profiling_results)
+
+
+class FrequencyScheduler(PowerStateScheduler):
+    """Interface for classes that enclose frequency scheduling policies."""
+
+    mode = "frequency"
+
+
+class PowerLimitScheduler(PowerStateScheduler):
+    """Interface for classes that enclose power limit scheduling policies."""
+
+    mode = "power limit"
+
+
+def make_3d_parallel(
+    sched_cls: Type[PowerStateScheduler], name: str | None = None
+) -> Type[PowerStateScheduler]:
+    """Wrap `sched_cls` so that it is aware of 3D parallelism.
+
+    Internally, this function subclasses `sched_cls` and overrides `observe` and
+    `next_schedule`. `observe` will aggregate the profiling results from all ranks
+    that share the same pp_rank and feed it to `super().observe`, while `next_schedule`
+    will first retrieve the per-stage schedule from `super().next_schedule` and then
+    copy-paste it to all ranks that share the same pp_rank. With this, the wrapped
+    scheduler can operate under the illusion that it's only dealing with pure pipeline
+    parallelism.
+
+    Args:
+        sched_cls: The scheduler class to wrap.
+        name: Name of the scheduler. If None, use `sched_cls.__name__ + "3D"`.
+    """
+
+    class Wrapper(sched_cls):  # type: ignore[valid-type,misc]
+        def __init__(
+            self,
+            job_info: JobInfo,
+            rank_infos: list[RankInfo],
+            perseus_settings: PerseusSettings,
+            *args,
+            **kwargs,
+        ) -> None:
+            self._orig_job_info = job_info
+            self._orig_rank_infos = rank_infos
+
+            # Give the wrapped scheduler a perfect illusion of pure pipeline parallelism
+            # and no data or tensor parallelism. New rank is given by pp_rank.
+            job_info = copy.deepcopy(job_info)
+            job_info.dp_degree = 1
+            job_info.tp_degree = 1
+            job_info.world_size = job_info.pp_degree
+
+            new_rank_infos = []
+            for rank_info in rank_infos:
+                if rank_info.dp_rank == 0 and rank_info.tp_rank == 0:
+                    new_rank_info = copy.deepcopy(rank_info)
+                    new_rank_info.rank = rank_info.pp_rank
+                    new_rank_infos.append(new_rank_info)
+
+            super().__init__(job_info, rank_infos, perseus_settings, *args, **kwargs)
+
+        def observe(self, profiling_results: list[ProfilingResult]) -> None:
+            """Aggregate results so that each pipeline stage has one result."""
+            # Aggregate results from ranks that share the same pp_rank.
+            rank_to_pp_rank = {
+                rank_info.rank: rank_info.pp_rank for rank_info in self._orig_rank_infos
+            }
+            pp_results: list[list[ProfilingResult]] = [
+                [] for _ in range(self._orig_job_info.pp_degree)
+            ]
+            for result in profiling_results:
+                pp_results[rank_to_pp_rank[result.rank]].append(result)
+
+            # For each stage, construct a new ProfilingResult that aggregates all ranks.
+            # For iter_time and values in time_breakdown, take the max.
+            # For iter_energy and values in energy_breakdown, take the sum.
+            def agg_list(values: Sequence[list[float]], fun: Callable) -> list[float]:
+                return [fun(vals) for vals in zip(*values)]
+
+            def agg_list_of_list(
+                values: Sequence[list[list[float]]], fun: Callable
+            ) -> list[list[float]]:
+                return [agg_list(vals, fun) for vals in zip(*values)]
+
+            agg_results = []
+            for pp_rank, results in enumerate(pp_results):
+                agg_result = ProfilingResult(
+                    rank=pp_rank,
+                    iter_time=agg_list([result.iter_time for result in results], max),
+                    iter_energy=agg_list(
+                        [result.iter_energy for result in results], sum
+                    ),
+                    time_breakdown={
+                        key: agg_list_of_list(
+                            [result.time_breakdown[key] for result in results], max
+                        )
+                        for key in results[0].time_breakdown
+                    },
+                    energy_breakdown={
+                        key: agg_list_of_list(
+                            [result.energy_breakdown[key] for result in results], sum
+                        )
+                        for key in results[0].energy_breakdown
+                    },
+                )
+                agg_results.append(agg_result)
+                logger.info(
+                    "Aggregated rank %s results for pp_rank %d: %s",
+                    ", ".join([str(r.rank) for r in results]),
+                    pp_rank,
+                    agg_result,
+                )
+
+            # Finally, let the wrapped scheduler observe the aggregated results.
+            super().observe(agg_results)
+
+        def next_schedule(self) -> list[PowerStateSchedule]:
+            """Copy and paste the schedule for each stage to all ranks in that stage."""
+            # Retrive the next schedule for each stage.
+            schedules = super().next_schedule()
+
+            # Copy and paste the schedule for each stage to all ranks in that stage.
+            rank_to_pp_rank = {
+                rank_info.rank: rank_info.pp_rank for rank_info in self._orig_rank_infos
+            }
+            next_schedule = []
+            for rank in range(self._orig_job_info.world_size):
+                pp_rank = rank_to_pp_rank[rank]
+                sched = copy.deepcopy(schedules[pp_rank])
+                sched.rank = rank
+                next_schedule.append(sched)
+                logger.info(
+                    "Copied schedule for pp_rank %d to rank %d: %s",
+                    pp_rank,
+                    rank,
+                    sched,
+                )
+            return next_schedule
+
+    Wrapper.__name__ = name or (sched_cls.__name__ + "3D")
+    if sched_cls.__doc__ is not None:
+        Wrapper.__doc__ = "[Wrapped for 3D parallelism] " + sched_cls.__doc__
+
+    return Wrapper
