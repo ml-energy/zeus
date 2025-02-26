f7e32fbb5ef833950a48c31384d45597c2a04b4e
DdIiVvYyAaMm <divyams@umich.edu>
2025-02-26 01:52:27 -0500

    init file

--- zeus/optimizer/pipeline_frequency/profiling/__init__.py
+++ zeus/optimizer/pipeline_frequency/profiling/__init__.py
@@ -1,5 +1,15 @@
-"Profiler for pipeline frequency tuning."
+"""Profiler for pipeline frequency tuning."""
 
 from zeus.optimizer.pipeline_frequency.profiling.profilers import InstructionProfiler
-from zeus.optimizer.pipeline_frequency.profiling.interfaces import PowerStateSchedulerV2, make_3d_parallel_perseus
-from zeus.optimizer.pipeline_frequency.profiling.models import PerseusSettings, JobInfoPerseus, RankInfoPerseus, PowerStateSchedule, ProfilingResultPerseus, PipeInstruction
+from zeus.optimizer.pipeline_frequency.profiling.interfaces import (
+    PowerStateSchedulerV2,
+    make_3d_parallel_perseus,
+)
+from zeus.optimizer.pipeline_frequency.profiling.models import (
+    PerseusSettings,
+    JobInfoPerseus,
+    RankInfoPerseus,
+    PowerStateSchedule,
+    ProfilingResultPerseus,
+    PipeInstruction,
+)
