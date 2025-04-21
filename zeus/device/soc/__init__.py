116b8fd39be4189c5713e26f11a158b05c82854d
Jisang Ahn <michahn@umich.edu>
2025-04-21 16:36:30 -0400

    Integrate Apple Silicon monitor object into SoC initializer

--- zeus/device/soc/__init__.py
+++ zeus/device/soc/__init__.py
@@ -17,17 +17,28 @@ def get_soc() -> SoC:
     The function returns a SoC management object that aims to abstract underlying SoC monitoring
     functionalities.
 
-    Currently, no management object has been implemented for any SoC architecture, so calling this
-    function will raise a `ZeusSoCInitError` error; implementations for SoC devices are expected
-    to be added in the near future.
+    Currently supported SoC devices:
+        - Apple Silicon
+
+    If no SoC monitor object can be initialized, a `ZeusSoCInitError` exception will be raised.
     """
     global _soc
     if _soc is not None:
         return _soc
 
-    # SoCs in the future can be incorporated via `elif` blocks.
-    else:
-        # Placeholder to avoid linting error (remove once _soc can be assigned a real value).
-        _soc = None
+    # --- Apple Silicon ---
+    try:
+        # The below import is done here instead of at top of file because the
+        # Apple Silicon module contains *optional* dependencies that will cause
+        # import failures if not installed on the host device.
+        from zeus.device.soc.apple import AppleSilicon, ZeusAppleInitError
 
+        _soc = AppleSilicon()
+    except (ImportError, ZeusSoCInitError):
+        pass
+
+    # For additional SoC's, add more initialization attempts.
+
+    if _soc is None:
         raise ZeusSoCInitError("No observable SoC was found on the current machine.")
+    return _soc
