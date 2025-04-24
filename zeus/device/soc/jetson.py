b645b99c2613afbff11cfefa0c89745e2b9a8205
jxunn <jxun@umich.edu>
2025-04-23 22:24:26 -0400

    tuning timeout

--- zeus/device/soc/jetson.py
+++ zeus/device/soc/jetson.py
@@ -263,7 +263,7 @@ async def _polling_process_async(
         try:
             command = await asyncio.wait_for(
                 asyncio.to_thread(command_queue.get),
-                timeout=3,
+                timeout=4,
             )
             print(f"Command received: {command}")
         except asyncio.TimeoutError:
