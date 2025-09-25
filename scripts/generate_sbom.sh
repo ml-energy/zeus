8f3fb935926f770da83dd1b1cfae1b54693b55e4
Jae-Won Chung <jwnchung@umich.edu>
2025-09-25 11:28:51 -0400

    Add scripts/generate_sbom.sh

---
+++ scripts/generate_sbom.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# This script requires uv to be installed. See https://docs.astral.sh/uv/.
+
+set -e
+
+uv sync --extra dev
+uv pip freeze | uv tool run --from cyclonedx-bom cyclonedx-py requirements --pyproject pyproject.toml --of JSON -o zeus-sbom.json -
